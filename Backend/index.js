2124349ab47ee61cb7e0179a4dd384f3e1d9714c
Naveen <naveenbaghel5429@gmail.com>
2024-05-08 16:41:14 +0530

    first change

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 const app = express();
 app.use(express.json());
 dotenv.config();
-mongoose.connect('mongodb+srv://naveen:naveen@cluster0.uoukw6g.mongodb.net/hotelManagement', {
+mongoose.connect('mongodb://localhost:27017/hotelmanagement', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
